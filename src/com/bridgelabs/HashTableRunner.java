ba6f7587c732a16a21304cce3fe81851c8e3c3f1
SamarthBM <samarthbm18@gmail.com>
2021-07-20 23:08:45 +0530

    Added: to find the frequency of sentence To be or not to be

--- src/com/bridgelabs/HashTableRunner.java
+++ src/com/bridgelabs/HashTableRunner.java
@@ -1,18 +1,35 @@
 /****************************************************************
- * Purpose : Initial commit to hash table program.
+ * Purpose :Ability to implement hashtable using Linked List
+ *          Ability to find frequency of words in a sentence like “To be or not to be”
  * @author Samarth BM
 
 ***************************************************************/
 
 package com.bridgelabs;
 
-
 public class HashTableRunner {
 
 	public static void main(String[] args) {
 
+		HashTableOperation<String, Integer> hashTable = new HashTableOperation<String, Integer>();
+		String sentence = "To be or not to be";
+
+		// Storing given sentence in an array.
+		// Converting the sentence to lower case and removing spaces.
+		String[] sentenceArray = sentence.toLowerCase().split(" ");
+
+		// Iterating over the array.
+		for (String word : sentenceArray) {
+			Integer count = hashTable.get(word);
+
+			if (count == null)
+				count = 1;
+			else
+				count = count + 1;
+			hashTable.add(word, count);
+		}
+		System.out.println(hashTable);
 
 	}
 
 }
-
